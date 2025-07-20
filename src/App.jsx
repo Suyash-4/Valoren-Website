8a72dd41eab7880463a86e02221ab5bf0d78306f
Suyash <mishrasuyash2018@gmail.com>
2025-07-20 20:17:02 +0530

    The first commit, basic setup done, tailwind setup!

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="p-10! ">
+        <div className="text-amber-400 text-4xl">App</div>
+      </div>
+    </>
+  );
+};
+
+export default App;
